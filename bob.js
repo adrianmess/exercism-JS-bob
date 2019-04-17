f0e407a8021f1e737394d4778528b804316c8c9e
Adrian Messado <adrian.messado@gmail.com>
2019-04-16 22:07:29 -0700

    add if statement to match empty string or only white spacce, change order of if statements

--- bob.js
+++ bob.js
@@ -4,7 +4,8 @@
 export const hey = (message) => {
   let messageArr = message.split("");
   if (message.includes("?")) { return "Sure."}
+  if (message.match("" || /^\s*$/g)) { return "Fine. Be that way!" }
   if (message.match(/^[^a-z]*$/g)) { return "Whoa, chill out!"}
-   else {return "no match"}
+  else { return "Whatever."}
 
 };
